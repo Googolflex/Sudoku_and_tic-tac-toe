52e1f036d0c9cc94e1c2e5507b263a754b17e0a4
Googolflex <googolplexian00@gmail.com>
2023-05-31 17:40:26 +0700

    3rd commit

--- Sudoku/Sudoku/Form1.Designer.cs
+++ Sudoku/Sudoku/Form1.Designer.cs
@@ -28,25 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // exit
-            // 
-            this.exit.Location = new System.Drawing.Point(520, 415);
-            this.exit.Name = "exit";
-            this.exit.Size = new System.Drawing.Size(75, 23);
-            this.exit.TabIndex = 3;
-            this.exit.Text = "Выход";
-            this.exit.UseVisualStyleBackColor = true;
-            this.exit.Click += new System.EventHandler(this.exit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(607, 450);
-            this.Controls.Add(this.exit);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +42,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button exit;
     }
 }
 
